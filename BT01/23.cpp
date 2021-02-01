dc302e0bf68b6ad0324661e59a891f3b81e72e6c
bluegillchad <78080659+bluegillchad@users.noreply.github.com>
2021-02-01 19:51:47 +0700

    Add files via upload

---
+++ BT01/23.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+    int x, y, tong, hieu;
+    cin >> x >> y;
+    tong = x + y;
+    hieu = abs(x - y);
+    cout << (tong + hieu) / 2;
+    return 0;
+}
+
+
